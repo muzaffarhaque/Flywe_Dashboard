9232f5e1e753e7be2c9e51ddc09c1392a963c5a9
muzaffarhaque <muzaffarhaque777@gmail.com>
2025-02-02 22:59:20 +0530

    minar changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,53 @@
 
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {Dashboard, ErrorPage, Home, Login, Profile, Roots} from './pages';
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import {Category, Dashboard, ErrorPage, Home, Login, Profile, Roots} from './pages';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import './App.scss'
+import NotFound from "./components/NotFound";
+
+const ProtectedRoute = ({ children }) => {
+    const token = localStorage.getItem('token_Flyweis');
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
+    return children;
+};
+
 
 function App() {
     const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <Login/>
-        },
+     
         {
             path: "/login",
             element: <Login/>
         },
         {
             path: "/profile",
-            element: <Profile/>
+            element: <ProtectedRoute children={<Profile/>}/>
         },
         {
             path:'/*',
             element:<ErrorPage/>
         },
         {
-            path: "/dashboard",
+            path: "/",
             element: <Roots/>,
-            errorElement:<ErrorPage/>,
+            errorElement:<NotFound/>,
             children:[
                 {
-                    path: "/dashboard",
-                    element: <Dashboard/>
+                    path: "/",
+                    element: <ProtectedRoute children={<Dashboard/>}/>
                 },
+                {
+                    path: "/dashboard",
+                    element: <ProtectedRoute children={<Dashboard/>}/>
+                },
+                {
+                    path: "/category",
+                    element: <ProtectedRoute children={<Category/>}/>
+                },
+               
             ]
         },
         
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,5 @@ export * from './dashboard';
 export * from './login';
 export * from './errorpage';
 export * from './profile';
+export * from './category';
 export {default as Home} from './Home';
